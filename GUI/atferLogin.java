bbf6950feab710770057bc56fd55d17ebf081903
fjnkhewgkfncsd <Mengly.heng@student.cadt.edu.kh>
2025-03-20 12:38:48 +0700

    add for staff

--- GUI/atferLogin.java
+++ GUI/atferLogin.java
@@ -1,10 +1,15 @@
 package GUI;
-import models.User;
+import models.*;
 
 public class atferLogin {
     public static void atferLoginFormGUI(User user){
         if(user.getRole().equals("Student")){
-            new Student_main_GUI();
+            if(user instanceof Student)
+            new Student_main_GUI((Student) user);
+        }
+        if(user.getRole().equals("Staff")){
+            if(user instanceof Staff)
+            new StaffForm((Staff) user);
         }
     }
 }
